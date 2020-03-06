597897b9e7d5602e08ce9ca9bfc7da0320ff48d4
dgant <116363+dgant@users.noreply.github.com>
2020-03-05 16:08:30 -0800

    Added validation for the deduplication

--- scripts/deduplicate.py
+++ scripts/deduplicate.py
@@ -1,4 +1,6 @@
 import argparse
+import math
+import random
 import typing
 from io import TextIOWrapper
 
@@ -6,7 +8,7 @@ def deduplicate_streams(input_src: TextIOWrapper, input_tgt: TextIOWrapper, outp
   """
   Deduplicates a parallel corpus.
   """
-  # Hashes of matched strings
+  # Hashes of strings we've encountered during deduplication
   matched_strings = set()
   
   # Step through both corpora
@@ -37,8 +39,46 @@ def deduplicate(input_src: str, input_tgt: str, output_src: str, output_tgt: str
   output_tgt_file = open(output_tgt, 'w')  
   deduplicate_streams(input_src_file, input_tgt=input_tgt_file, output_src=output_src_file, output_tgt=output_tgt_file)
 
-def validate_alignment(input_src: str, input_tgt: str, output_src: str, output_tgt: str) -> bool:
-  pass
+def count_lines(stream):
+  return sum(1 for line in stream)
+  
+def validate_alignment(input_src: str, input_tgt: str, output_src: str, output_tgt: str):
+  """
+  Spot-checks original and deduplicated corpora to verify that text alignment was preserved during deduplication.
+  """
+  input_src_file_length = count_lines(open(input_src, 'r'))
+  input_tgt_file_length = count_lines(open(input_tgt, 'r'))
+  output_src_file_length = count_lines(open(output_src, 'r'))
+  output_tgt_file_length = count_lines(open(output_tgt, 'r'))
+  assert(input_src_file_length == input_tgt_file_length)
+  assert(output_src_file_length == output_tgt_file_length)  
+  input_src_file = open(input_src, 'r')
+  input_tgt_file = open(input_tgt, 'r')
+  output_src_file = open(output_src, 'r')
+  output_tgt_file = open(output_tgt, 'r')
+  # Validate alignment for a random batch of lines from the output corpora
+  max_skip = int(math.sqrt(output_src_file_length))
+  line_now = 0
+  line_next = 0
+  for output_line_src, output_line_tgt in zip(output_src_file, output_tgt_file):
+    if line_now == line_next:      
+      line_next = line_now + random.randint(1, max_skip)
+      while True:
+        input_line_src = input_src_file.readline()
+        input_line_tgt = input_tgt_file.readline()
+        if len(input_line_src) == 0 or len(input_line_tgt) == 0:
+          raise Exception(f"Didn't find line in input corpus matching:\nSource:\n{output_line_src}\nTarget:\n{output_line_tgt}")
+        matched_src = input_line_src == output_line_src
+        matched_tgt = input_line_tgt == output_line_tgt
+        if matched_src and matched_tgt:
+          print(f"Found correct alignment for ({input_line_src[0:16].rstrip()}..., {input_line_tgt[0:16].rstrip()}...)")
+          break
+        if matched_src or matched_tgt:
+          raise Exception(f"Found misalignment.\nInput source:\n{input_line_src}\nInput target:\n{input_line_tgt}\nOutput source:\n{output_line_src}\nOutput target:\n{output_line_tgt}")
+        
+    line_now += 1
+  #for input_line_src, input_line_tgt, output_line_src, output_line_tgt in zip(open(input_src), open(input_tgt), open(output_src), open(output_tgt)):
+  
   
 def main():
   parser = argparse.ArgumentParser()
